92770c5b4c4b9afca349166ef9f9e8068bb94c98
merlinymy <lordhorcrux@gmail.com>
2025-09-22 17:05:03 -0700

    Add skill section, finish landing page

---
+++ scripts/skillsGrid.js
@@ -0,0 +1,31 @@
+const buildSkillCard = (skillSet) => {
+  const skillsCard = document.createElement("div");
+  skillsCard.classList.add("skill-card");
+  const cardTitle = document.createElement("h3");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = skillSet.group;
+  const skCardsContainer = document.createElement("div");
+  skCardsContainer.classList.add("sk-container");
+  console.log(skillSet);
+  skillSet.skills.map((skill) => {
+    const skCard = document.createElement("div");
+    skCard.classList.add("sk-card");
+    const icon = document.createElement("div");
+    icon.classList.add("skill-icon");
+    icon.innerHTML = skill.svg;
+    const skillName = document.createElement("p");
+    skillName.classList.add("skill-name");
+    skillName.textContent = skill.name;
+    skCard.append(icon, skillName);
+    skCardsContainer.append(skCard);
+  });
+  skillsCard.append(cardTitle, skCardsContainer);
+  return skillsCard;
+};
+
+const populateSkillGrid = (skillSets) => {
+  const skillSection = document.querySelector(".skill-grid");
+  skillSection.append(...skillSets);
+};
+
+export { buildSkillCard, populateSkillGrid };
