e3805f394d2882207a9fc95fcd3524cbc53f0b69
merlinymy <lordhorcrux@gmail.com>
2025-09-22 17:20:17 -0700

    Fix image path for deployment

--- assets/projectsArray.js
+++ assets/projectsArray.js
@@ -1,6 +1,16 @@
+import tcg from "../assets/projectImg/tcg.png";
+import smgen from "../assets/projectImg/smgen.png";
+import fireproofsheep from "../assets/projectImg/fireproofship.png";
+import rainbowSphere from "../assets/projectImg/rainbowSphere.png";
+import resumeBuilder from "../assets/projectImg/resumeBuilder.png";
+import binaryTree from "../assets/projectImg/binaryTree.png";
+import bubble from "../assets/projectImg/bubble.png";
+import weatherapp from "../assets/projectImg/weatherapp.png";
+import todo from "../assets/projectImg/todo.png";
+
 export const projectArr = [
   {
-    imgUrl: "../assets/projectImg/tcg.png",
+    imgUrl: tcg,
     name: "Pokemon TCG Memory League",
     desc: "A collectible twist on the classic memory game, featuring Pokémon TCG cards and progressive challenges.",
     tech: ["React", "Tailwind", "Howler.js", "Vite"],
@@ -10,21 +20,21 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/smgen.png",
+    imgUrl: smgen,
     name: "Social Media Post Generator",
     desc: "An AI-powered social media post generator with multilingual support, business info management, and a personalized dashboard.",
     tech: ["React", "Tailwind", "Supabase", "Cursor"],
     links: ["https://fantastmediagen.vercel.app/login"],
   },
   {
-    imgUrl: "../assets/projectImg/fireproofship.png",
+    imgUrl: fireproofsheep,
     name: "Fireproof Sheep",
     desc: "A top-down shooter web game built with PixiJS. With a unique wool-based resource system that powers both offense and defense.",
     tech: ["Pixi.js", "Claude Agent"],
     links: ["https://fire-proof-sheep.vercel.app/"],
   },
   {
-    imgUrl: "../assets/projectImg/rainbowSphere.png",
+    imgUrl: rainbowSphere,
     name: "Giant Interactive 3D Sphere",
     desc: "A dynamic, layered triangle turning into a sphere visualized in the browser that blends geometry, animation, and interaction.",
     tech: ["Three.js"],
@@ -34,7 +44,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/resumeBuilder.png",
+    imgUrl: resumeBuilder,
     name: "ATS Friendly Resume Builder",
     desc: "A modern, printable, and ATS-safe resume builder built with React.",
     tech: ["React", "Tailwind"],
@@ -44,7 +54,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/binaryTree.png",
+    imgUrl: binaryTree,
     name: "Binary Tree Visualizer",
     desc: "A retro-styled browser app to visualize, edit, and explore binary search trees in real time.",
     tech: ["Javascript", "css", "html"],
@@ -54,7 +64,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/bubble.png",
+    imgUrl: bubble,
     name: "Bubble Bar Restaurant Page",
     desc: "Bubble Bar is a fictional restaurant website featuring a Star Trek-inspired menu and a custom bubble rising animation created with HTML Canvas. ",
     tech: ["Javascript", "css", "html", "canvas"],
@@ -64,7 +74,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/weatherapp.png",
+    imgUrl: weatherapp,
     name: "iOS-styled Weather App",
     desc: "A web replica of Apple’s Weather app focused on a clean, responsive iOS-style UI.",
     tech: ["Javascript", "css", "html"],
@@ -74,7 +84,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "../assets/projectImg/todo.png",
+    imgUrl: todo,
     name: "iOS-styled Todo List",
     desc: "A lightweight, iOS-inspired reminder app, built entirely with JavaScript, CSS, and HTML—no frameworks, no libraries. This project is a testament to my capability of crafting a smooth, iOS-like experience using only vanilla web technologies.",
     tech: ["Javascript", "css", "html"],
