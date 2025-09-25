1c86a60ce6b4efca7a0d68d68e1ca79e65eff00c
merlinymy <lordhorcrux@gmail.com>
2025-09-25 13:56:44 -0700

    Fix file type

--- assets/projectsArray.js
+++ assets/projectsArray.js
@@ -1,6 +1,6 @@
 export const projectArr = [
   {
-    imgUrl: "./assets/project_web/tcg.png",
+    imgUrl: "./assets/project_web/tcg.webp",
     name: "Pokemon TCG Memory League",
     desc: "A collectible twist on the classic memory game, featuring Pokémon TCG cards and progressive challenges.",
     tech: ["React", "Tailwind", "Howler.js", "Vite"],
@@ -10,21 +10,21 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/smgen.png",
+    imgUrl: "./assets/project_web/smgen.webp",
     name: "Social Media Post Generator",
     desc: "An AI-powered social media post generator with multilingual support, business info management, and a personalized dashboard.",
     tech: ["React", "Tailwind", "Supabase", "Cursor"],
     links: ["https://fantastmediagen.vercel.app/login"],
   },
   {
-    imgUrl: "./assets/project_web/fireproofship.png",
+    imgUrl: "./assets/project_web/fireproofship.webp",
     name: "Fireproof Sheep",
     desc: "A top-down shooter web game built with PixiJS. With a unique wool-based resource system that powers both offense and defense.",
     tech: ["Pixi.js", "Claude Agent"],
     links: ["https://fire-proof-sheep.vercel.app/"],
   },
   {
-    imgUrl: "./assets/project_web/rainbowSphere.png",
+    imgUrl: "./assets/project_web/rainbowSphere.webp",
     name: "Giant Interactive 3D Sphere",
     desc: "A dynamic, layered triangle turning into a sphere visualized in the browser that blends geometry, animation, and interaction.",
     tech: ["Three.js"],
@@ -34,7 +34,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/resumeBuilder.png",
+    imgUrl: "./assets/project_web/resumeBuilder.webp",
     name: "ATS Friendly Resume Builder",
     desc: "A modern, printable, and ATS-safe resume builder built with React.",
     tech: ["React", "Tailwind"],
@@ -44,7 +44,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/binaryTree.png",
+    imgUrl: "./assets/project_web/binaryTree.webp",
     name: "Binary Tree Visualizer",
     desc: "A retro-styled browser app to visualize, edit, and explore binary search trees in real time.",
     tech: ["Javascript", "css", "html"],
@@ -54,7 +54,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/bubble.png",
+    imgUrl: "./assets/project_web/bubble.webp",
     name: "Bubble Bar Restaurant Page",
     desc: "Bubble Bar is a fictional restaurant website featuring a Star Trek-inspired menu and a custom bubble rising animation created with HTML Canvas. ",
     tech: ["Javascript", "css", "html", "canvas"],
@@ -64,7 +64,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/weatherapp.png",
+    imgUrl: "./assets/project_web/weatherapp.webp",
     name: "iOS-styled Weather App",
     desc: "A web replica of Apple’s Weather app focused on a clean, responsive iOS-style UI.",
     tech: ["Javascript", "css", "html"],
@@ -74,7 +74,7 @@ export const projectArr = [
     ],
   },
   {
-    imgUrl: "./assets/project_web/todo.png",
+    imgUrl: "./assets/project_web/todo.webp",
     name: "iOS-styled Todo List",
     desc: "A lightweight, iOS-inspired reminder app, built entirely with JavaScript, CSS, and HTML—no frameworks, no libraries. This project is a testament to my capability of crafting a smooth, iOS-like experience using only vanilla web technologies.",
     tech: ["Javascript", "css", "html"],
